4ce0e29ff2fdae936988e518849be3e2aa628faa
vm2 <maciejk1407@gmail.com>
2024-11-19 12:01:25 +0100

    test

--- lab5/reciever.c
+++ lab5/reciever.c
@@ -11,75 +11,55 @@
 #define SA struct sockaddr
 #define MAXLINE 1024
 
-int snd_udp_socket(const char *serv, int port, SA **saptr, socklen_t *lenp)
-{
-	int sockfd, n;
-	struct addrinfo	hints, *res, *ressave;
-	struct sockaddr_in6 *pservaddrv6;
-	struct sockaddr_in *pservaddrv4;
+int snd_udp_socket(const char *serv, int port, SA **saptr, socklen_t *lenp) {
+    int sockfd;
+    struct sockaddr_in *pservaddr;
 
-	*saptr = malloc( sizeof(struct sockaddr_in6));
-	
-	pservaddrv6 = (struct sockaddr_in6*)*saptr;
+    *saptr = malloc(sizeof(struct sockaddr_in));
+    if (*saptr == NULL) {
+        perror("malloc error");
+        return -1;
+    }
 
-	bzero(pservaddrv6, sizeof(struct sockaddr_in6));
+    pservaddr = (struct sockaddr_in *)*saptr;
+    memset(pservaddr, 0, sizeof(struct sockaddr_in));
 
-	if (inet_pton(AF_INET6, serv, &pservaddrv6->sin6_addr) <= 0){
-	
-		free(*saptr);
-		*saptr = malloc( sizeof(struct sockaddr_in));
-		pservaddrv4 = (struct sockaddr_in*)*saptr;
-		bzero(pservaddrv4, sizeof(struct sockaddr_in));
+    pservaddr->sin_family = AF_INET;
+    pservaddr->sin_port = htons(port);
 
-		if (inet_pton(AF_INET, serv, &pservaddrv4->sin_addr) <= 0){
-			fprintf(stderr,"AF_INET inet_pton error for %s : %s \n", serv, strerror(errno));
-			return -1;
-		}else{
-			pservaddrv4->sin_family = AF_INET;
-			pservaddrv4->sin_port   = htons(port);
-			*lenp =  sizeof(struct sockaddr_in);
-			if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0){
-				fprintf(stderr,"AF_INET socket error : %s\n", strerror(errno));
-				return -1;
-			}
-		}
+    if (inet_pton(AF_INET, serv, &pservaddr->sin_addr) <= 0) {
+        fprintf(stderr, "inet_pton error for %s: %s\n", serv, strerror(errno));
+        free(*saptr);
+        return -1;
+    }
 
-	}else{
-		pservaddrv6 = (struct sockaddr_in6*)*saptr;
-		pservaddrv6->sin6_family = AF_INET6;
-		pservaddrv6->sin6_port   = htons(port);	/* daytime server */
-		*lenp =  sizeof(struct sockaddr_in6);
+    *lenp = sizeof(struct sockaddr_in);
 
-		if ( (sockfd = socket(AF_INET6, SOCK_DGRAM, 0)) < 0){
-			fprintf(stderr,"AF_INET6 socket error : %s\n", strerror(errno));
-			return -1;
-		}
+    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+        perror("socket error");
+        free(*saptr);
+        return -1;
+    }
 
-	}
-
-	return(sockfd);
+    return sockfd;
 }
 
-int mcast_join(int sockfd, const SA *grp, socklen_t grplen,
-		   const char *ifname, u_int ifindex)
-{
-	struct group_req req;
-	if (ifindex > 0) {
-		req.gr_interface = ifindex;
-	} else if (ifname != NULL) {
-		if ( (req.gr_interface = if_nametoindex(ifname)) == 0) {
-			errno = ENXIO;	/* if name not found */
-			return(-1);
-		}
-	} else
-		req.gr_interface = 0;
-	if (grplen > sizeof(req.gr_group)) {
-		errno = EINVAL;
-		return -1;
-	}
-	memcpy(&req.gr_group, grp, grplen);
-	return (setsockopt(sockfd, family_to_level(grp->sa_family),
-			MCAST_JOIN_GROUP, &req, sizeof(req)));
+int mcast_join(int sockfd, const SA *grp, socklen_t grplen, const char *ifname, u_int ifindex) {
+    struct ip_mreq mreq;
+
+    memcpy(&mreq.imr_multiaddr,
+           &((struct sockaddr_in *)grp)->sin_addr,
+           sizeof(struct in_addr));
+
+    if (ifname != NULL) {
+        struct in_addr local_interface;
+        local_interface.s_addr = inet_addr(ifname);
+        mreq.imr_interface = local_interface;
+    } else {
+        mreq.imr_interface.s_addr = htonl(INADDR_ANY);
+    }
+
+    return setsockopt(sockfd, IPPROTO_IP, IP_ADD_MEMBERSHIP, &mreq, sizeof(mreq));
 }
 
 void recv_all(int recvfd, socklen_t salen) {
