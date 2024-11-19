ee790d82a56d8d858786d6797025b3dc1f14ebb1
vm2 <maciejk1407@gmail.com>
2024-11-19 11:57:07 +0100

    fixed ?

--- lab5/sender.c
+++ lab5/sender.c
@@ -11,8 +11,76 @@
 
 #define MAXLINE 1024
 #define SA struct sockaddr
-int snd_udp_socket(const char *serv, int port, SA **saptr, socklen_t *lenp);
-int mcast_join(int sockfd, const SA *grp, socklen_t grplen, const char *ifname, u_int ifindex);
+int snd_udp_socket(const char *serv, int port, SA **saptr, socklen_t *lenp)
+{
+	int sockfd, n;
+	struct addrinfo	hints, *res, *ressave;
+	struct sockaddr_in6 *pservaddrv6;
+	struct sockaddr_in *pservaddrv4;
+
+	*saptr = malloc( sizeof(struct sockaddr_in6));
+	
+	pservaddrv6 = (struct sockaddr_in6*)*saptr;
+
+	bzero(pservaddrv6, sizeof(struct sockaddr_in6));
+
+	if (inet_pton(AF_INET6, serv, &pservaddrv6->sin6_addr) <= 0){
+	
+		free(*saptr);
+		*saptr = malloc( sizeof(struct sockaddr_in));
+		pservaddrv4 = (struct sockaddr_in*)*saptr;
+		bzero(pservaddrv4, sizeof(struct sockaddr_in));
+
+		if (inet_pton(AF_INET, serv, &pservaddrv4->sin_addr) <= 0){
+			fprintf(stderr,"AF_INET inet_pton error for %s : %s \n", serv, strerror(errno));
+			return -1;
+		}else{
+			pservaddrv4->sin_family = AF_INET;
+			pservaddrv4->sin_port   = htons(port);
+			*lenp =  sizeof(struct sockaddr_in);
+			if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0){
+				fprintf(stderr,"AF_INET socket error : %s\n", strerror(errno));
+				return -1;
+			}
+		}
+
+	}else{
+		pservaddrv6 = (struct sockaddr_in6*)*saptr;
+		pservaddrv6->sin6_family = AF_INET6;
+		pservaddrv6->sin6_port   = htons(port);	/* daytime server */
+		*lenp =  sizeof(struct sockaddr_in6);
+
+		if ( (sockfd = socket(AF_INET6, SOCK_DGRAM, 0)) < 0){
+			fprintf(stderr,"AF_INET6 socket error : %s\n", strerror(errno));
+			return -1;
+		}
+
+	}
+
+	return(sockfd);
+}
+
+int mcast_join(int sockfd, const SA *grp, socklen_t grplen,
+		   const char *ifname, u_int ifindex)
+{
+	struct group_req req;
+	if (ifindex > 0) {
+		req.gr_interface = ifindex;
+	} else if (ifname != NULL) {
+		if ( (req.gr_interface = if_nametoindex(ifname)) == 0) {
+			errno = ENXIO;	/* if name not found */
+			return(-1);
+		}
+	} else
+		req.gr_interface = 0;
+	if (grplen > sizeof(req.gr_group)) {
+		errno = EINVAL;
+		return -1;
+	}
+	memcpy(&req.gr_group, grp, grplen);
+	return (setsockopt(sockfd, family_to_level(grp->sa_family),
+			MCAST_JOIN_GROUP, &req, sizeof(req)));
+}
 
 void send_all(int sendfd, SA *sadest, socklen_t salen) {
     char line[MAXLINE];
